6eddf9769dbec23779fe624e0961302af9fbff84
juliely8619 <july8619@yahoo.com>
2018-10-11 23:45:58 -0700

    wrote outlines

---
+++ app.js
@@ -0,0 +1,41 @@
+//building the outline//
+//add blurb about purpose of this code: 
+  //what information will we take in
+  //what do we do with the information
+  //why are we doing this, what is it for
+
+//=======global variables======//
+//image 1
+  //previous selection 1
+//image 2
+  //previous selection 2
+//image 3
+  //previous selection 3
+//array of all the images
+
+//=======Construction function======//
+//build the object: needs to contain the following info
+  //image source
+  //title for image
+  //likes count
+  //appearance count
+  //add object to all images array
+
+//=======Functions======//
+
+//choose a random image, that isn't one of the other 3 and wasn't just on screen before
+
+//handler function
+  //check they clicked on an image
+  //calls the random for all 3 images
+  //increase likes
+  //increase appeared
+  //update assigning the current image
+  //this will be for lab 12, but add count to render chart and stop listener
+
+
+//calling the handling for click
+
+//create the objects
+
+
